9e4e999f42d87529bb43170dfbfe65d77885d64c
Ayush jyoti <ayushjyoti0321@gmail.com>
2025-06-15 04:07:43 +0530

    redis deployed on railway

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,24 @@
 // index.js or server.js
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import chatRoutes from "./routes/chat.js"; // make sure extension is .js
-
+import chatRoutes from "./routes/chat.js";
 connectDB();
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json()); // optional, express.json() is enough
+app.use(cors({
+    origin: 'https://infopilot-lilac.vercel.app', // frontend URL
+}));
 app.use(express.json());
 
 app.use("/api/chat", chatRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+app.get('/health', (req, res) => {
+    res.json({ status: 'ok', message: 'QueryHub backend is running!' });
+});
